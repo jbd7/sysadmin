be5f876a720ef974600ff80256c02212ea431c4b
jbd7 <jbdfr-git@yahoo.com>
2022-01-29 14:42:48 +0100

    Update index.php

--- tomato/index.php
+++ tomato/index.php
@@ -4,3 +4,40 @@
 <a href="devices.asp">devices</a>
 <a href="phpinfo.php">phpinfo</a>
 <a href="index.html">nginx homepage</a>
+<html>
+        <head>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        </head>
+
+<body>        
+
+        <?php
+           function prep2print($filename) {
+                return nl2br(file_get_contents($filename));
+            }
+        ?>
+
+<h1><a href="syslog.php">syslog</a></h1>
+<br /><br />
+
+<h3>
+</h3>
+<br /><br />
+
+<hr5>
+<a href="devices.asp">devices</a>
+<a href="phpinfo.php">phpinfo</a>
+<a href="index2.html">nginx homepage</a>
+</h5>
+<br /><br />
+
+<h4>hosts.dnsmasq</h4>
+        <?php
+           echo prep2print('hosts.dnsmasq');
+        ?>
+
+
+</body>
+</html>

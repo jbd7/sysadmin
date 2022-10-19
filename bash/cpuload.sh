3e4cf563473cdc13d666b908e288eb501dba6128
jbd7 <jbdfr-git@yahoo.com>
2022-10-19 16:54:47 +0200

    Add parameters for cpuload.sh

--- bash/cpuload.sh
+++ bash/cpuload.sh
@@ -2,23 +2,31 @@
 ## FORKED FROM from https://github.com/techarkit/CPU_Utilization_Script on 221019
 ## Collect Real Time CPU Usage DATE: 12th July 2017 Author: Ankam Ravi Kumar
 ## 2016 is the number of 5min blocks into 1 week, so as to truncate the file when on a cron with a 5min frequency
+
+## Where to send alerts
 EMAILADDRESS=<INSERT_EMAIL_ADDRESS_HERE>
+
+## Threshold in percent above which the 15min average is declared a stress
+STRESSLIMIT=150
+
+## Threshold in percent above which the 1min average is declared a spike
+SPIKELIMIT=1000
 
 CPU_USAGE=$(cat /proc/loadavg | awk '{print $1 " " $2 " " $3}')
 CPU_USAGE_15AVG=$(cat /proc/loadavg | awk '{print $3 * 100}')
 CPU_USAGE_01AVG=$(cat /proc/loadavg | awk '{print $1 * 100}')
-if [ $CPU_USAGE_15AVG -ge 1 ]; then
+if [ $CPU_USAGE_15AVG -ge $STRESSLIMIT ]; then
  DATE=$(date "+%F %H:%M:%S")
  CPU_USAGE1="$DATE CPU: $CPU_USAGE"
- echo "STRESS $CPU_USAGE1" >> /tmp/cpusage.out
+ echo "STRESS \t $CPU_USAGE1" >> /tmp/cpusage.out
  cat /tmp/cpusage.out |tail -5 > /tmp/cpusage.tmp
  echo "" >> /tmp/cpusage.tmp
  echo $(ps auxh --sort=-c | awk 'NR<=5 {printf "ps:  %5s %6d %s\r\n",$3,$2,$11}') >> /tmp/cpusage.tmp
  mail -s "CPU Utilization (Continous stess) of `hostname`" $EMAILADDRESS < /tmp/cpusage.tmp
-elif [ $CPU_USAGE_01AVG -ge 500 ]; then
+elif [ $CPU_USAGE_01AVG -ge $SPIKELIMIT ]; then
  DATE=$(date "+%F %H:%M:%S")
  CPU_USAGE1="$DATE CPU: $CPU_USAGE"
- echo "SPIKE  $CPU_USAGE1" >> /tmp/cpusage.out
+ echo "SPIKE \t $CPU_USAGE1" >> /tmp/cpusage.out
  cat /tmp/cpusage.out |tail -5 > /tmp/cpusage.tmp
  echo "" >> /tmp/cpusage.tmp
  echo $(ps auxh --sort=-c | awk 'NR<=5 {printf "ps:  %5s %6d %s\r\n",$3,$2,$11}') >> /tmp/cpusage.tmp
@@ -26,6 +34,7 @@ elif [ $CPU_USAGE_01AVG -ge 500 ]; then
 else
  DATE=$(date "+%F %H:%M:%S")
  CPU_USAGE1="$DATE CPU: $CPU_USAGE"
- echo "OK     $CPU_USAGE1" >> /tmp/cpusage.out
- cat /tmp/cpusage.out |tail -2016 > /tmp/cpusage.out
+ echo "OK \t $CPU_USAGE1" >> /tmp/cpusage.out
+ cat /tmp/cpusage.out |tail -2016 > /tmp/cpusage.tmp
+ mv /tmp/cpusage.tmp /tmp/cpusage.out
 fi
